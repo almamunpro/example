594de3762f1ed33879493cbc43177d79cec6a307
Abdullah al mamun <61002570+stranger-1-1-13@users.noreply.github.com>
2025-01-08 21:15:45 +0600

    SQLite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Home</title>
+    <title>{{$heading}}</title>
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
