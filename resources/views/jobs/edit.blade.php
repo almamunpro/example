ac59224155a26a15713a4cfe1dd00ded711ef37a
Abdullah al mamun <61002570+stranger-1-1-13@users.noreply.github.com>
2025-01-26 08:43:07 +0600

    edit update delete show

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -2,9 +2,9 @@
     <x-slot:heading>
         Edit Job: {{ $job->title}}
     </x-slot:heading>
-    <form method="POST" action="/jobs/index">
+    <form method="POST" action="/jobs/index/ {{$job->id}}">
         @csrf
-
+        @method('PATCH')
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base/7 font-semibold text-gray-900">Create a New Job</h2>
--- routes/web.php
+++ routes/web.php
@@ -56,20 +56,40 @@ Route::post('/jobs/index', function () {
     return redirect('/jobs/index');
 });
 
-// Update
-// Route::patch('/jobs/{id}/edit', function ($id)  {
-//     $job = Job::find($id);
-//     return view('jobs.edit', ['job' => $job]);
-// });
-Route::patch('/jobs/{id}/edit', function ($id)  {
+
+Route::get('/jobs/{id}/edit', function ($id)  {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+Route::patch('/jobs/index/{id}/edit', function ($id)  {
     // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary'=>['required']
+    ]);
     //authorize (ON hold)
     //update the job
+    $job= Job::findOrFail($id); //null
+
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
     //and persist
+    return redirect('/jobs/index '. $job ->id);
 
 });
 // destroy
 Route::delete('/jobs/{id}/edit', function ($id)  {
-    $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
+    // authorize (On Hold)
+
+    //delete the job
+    Job::findOrFail($id)->delete();
+
+    // redirect
+    return redirect('/jobs/index/');
 });
